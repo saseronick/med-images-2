fca8e7dfc38063c3557a6e83ab0945058bf6c666
Stacey <sseronic@uci.edu>
2017-04-22 21:34:50 -0700

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,10 @@
+ <?php
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+    // FIXME: add more validation, e.g. using ext/fileinfo
+    try {
+        $upload = $s3->upload($bucket, $_FILES['userfile']['filename'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+?>
+        <p>Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)</p>
+<?php } catch(Exception $e) { ?>
+        <p>Upload error :(</p>
+<?php } } ?>
